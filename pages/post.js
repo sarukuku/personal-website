8242ff6bba9651f042149e334f0f55195830b15e
sarukuku <joonas.salovaara@gmail.com>
2017-05-25 20:41:55 +0300

    Removed node_modules, don't even know why tey slipped in. Also added data fetching for posts.

--- pages/post.js
+++ pages/post.js
@@ -5,35 +5,28 @@ var WPAPI = require('wpapi')
 var wp = new WPAPI({ endpoint: 'https://api.jsalovaara.com/wp-json' })
 
 export default class extends React.Component {
-  static async getInitialProps () {
-    const posts = await wp.posts().perPage(100).then(data => {
+  static async getInitialProps ({query}) {
+    const { id } = query
+    const post = await wp.posts().id(id).then(data => {
       return data
     }).catch(err => {
       console.log(err)
     })
 
-    return { posts: posts }
+    return { post: post }
   }
 
   render () {
-    let { posts } = this.props
+    let { post } = this.props
+
+    console.log(post)
 
     return (
       <div className='Blog'>
         <SiteHead title='Blog of Joonas Salovaara' />
         <Link prefetch href='/'><a>Home</a></Link>
-        {posts.map(post => {
-          return (
-            <Link prefetch key={post.id} href={{pathname: 'post', query: { id: post.id }}}>
-              <a dangerouslySetInnerHTML={{__html: post.title.rendered}} />
-            </Link>
-          )
-        })}
-        <style jsx>{`
-          a {
-            display: block;
-          }
-        `}</style>
+        <Link prefetch href='/blog'><a>Blog</a></Link>
+        <section dangerouslySetInnerHTML={{__html: post.content.rendered}} />
       </div>
     )
   }
