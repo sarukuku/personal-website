2285ea88e907f3ede5448208b46c2eae0f957491
Joonas Salovaara <joonas.salovaara@gmail.com>
2017-07-24 17:09:37 +0200

    New build.

--- out/_next/4b368461-0943-49bb-85f0-f77eb69b8b78/page/blog/index.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/blog', function() {
-            var comp = module.exports=webpackJsonp([2],[],[218]);
-            return { page: comp.default }
-          })
-        
