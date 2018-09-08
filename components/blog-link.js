154f01207364dd03dc5fc5251390c45de6bcc1bd
Joonas Salovaara <joonas.salovaara@gmail.com>
2018-09-08 22:48:52 +0200

    Update deps & texts

--- components/blog-link.js
+++ components/blog-link.js
@@ -1,19 +1,17 @@
 import Link from 'next/link'
 
-export default () => (
-  <span>
-    <Link prefetch href='/blog'><a className='BlogLink'>read my blog</a></Link>
-    <style jsx global>{`
-      .BlogLink:after {
-        display: inline-block;
-        content: '📋';
-        margin-left: .2rem;
-        transition: all .2s ease-in-out;
-      }
+export default ({ text }) => (<span>
+  <Link prefetch href='/blog'><a className='BlogLink'>{text}</a></Link>
+  <style jsx global>{`
+    .BlogLink:after {
+      display: inline-block;
+      content: '📋';
+      margin-left: .2rem;
+      transition: all .2s ease-in-out;
+    }
 
-      .BlogLink:hover:after {
-        transform: rotate(10deg) scale(1.2);
-      }
-    `}</style>
-  </span>
-)
+    .BlogLink:hover:after {
+      transform: rotate(10deg) scale(1.2);
+    }
+  `}</style>
+</span>)
